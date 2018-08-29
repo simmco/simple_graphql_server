f7a0732cba47f88f5e98a7f788086e1a1d2134ea
simmco <m.simmelbauer@gmail.com>
2018-08-29 19:56:24 +0200

    setup server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
-import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-console.log('Hello Node.js project.');
+const app = express();
 
-console.log(process.env.MY_DATABASE_PASSWORD);
+app.use(cors());
+
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Robin Wieruch',
+      };
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
