45c4041b7f19697b748724ef563abf5e9f3bab67
simmco <m.simmelbauer@gmail.com>
2018-08-31 18:46:30 +0200

    setup sequilize with db

--- src/models/index.js
+++ src/models/index.js
@@ -1,35 +1,26 @@
-let users = {
-  1: {
-    id: '1',
-    username: 'Robin Wieruch',
-    messageIds: [1],
-  },
-  2: {
-    id: '2',
-    username: 'Dave Davids',
-    messageIds: [2],
-  },
-  3: {
-    id: '3',
-    username: 'Michael Simmelbauer',
-    messageIds: [],
-  },
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    dialect: 'postgres',
+    port: '5432',
+  }
+);
+
+const models = {
+  User: sequelize.import('./user'),
+  Message: sequelize.import('./message'),
 };
 
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2',
-  },
-};
+Object.keys(models).forEach(key => {
+  if ('associate' in models[key]) {
+    models[key].associate(models);
+  }
+});
 
-export default {
-  users,
-  messages,
-};
+export { sequelize };
+
+export default models;
